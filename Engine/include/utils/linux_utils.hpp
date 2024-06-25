6453efb3e86b505af7077b97b7eb6c5629051f93
RS2 <96263419+juice2011@users.noreply.github.com>
2024-06-25 12:27:21 -0400

    Update linux_utils.hpp

--- Engine/include/utils/linux_utils.hpp
+++ Engine/include/utils/linux_utils.hpp
@@ -1,1 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <wchar.h>
+
 #define DOCUMENT_PATH "~/Documents"
+#define WCTMB(x, y, z) wcstombs(x, y, z)
+#define NOTIFY(x, y) system("notify-send [%s] %s", x, y)
